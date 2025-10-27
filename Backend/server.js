bcb507fe39aecccf8313b732137b9d061ecade94
RedUstine <redpupo@gmail.com>
2025-10-27 06:08:48 -0600

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,16 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const API_BASE_URL = process.env.API_BASE_URL || `http://localhost:${PORT}`;
 const { join } = require("path");
+const frontendBuildPath = path.join(__dirname, "../frontend/build");
 // --- Middleware ---
 app.use(cors());
 app.use(express.json());
+app.use(express.static(frontendBuildPath));
 
 console.log("Mongo URI:", process.env.MONGO_URI);
 
@@ -84,9 +86,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(frontendBuildPath));
 
     // Catch-all route to serve index.html for React Router
-    // app.get("*", (req, res) => {
-    //   res.sendFile(path.join(frontendBuildPath, "index.html"));
-    // });
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(frontendBuildPath, "index.html"));
+    });
 
     console.log("✅ Frontend static serving enabled");
   } else {
