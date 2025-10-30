04f1b5f47311ef13c423bd21941739b3acd851e1
RedUstine <redpupo@gmail.com>
2025-10-30 04:13:48 -0600

    update API URL

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -29,7 +29,7 @@ const DHLLoginPage = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch(`${API_BASE_URL}/login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
